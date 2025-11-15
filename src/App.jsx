35d47ae6ebf968a7de65178e7554800c10074ed3
RahmaAribi <rahmaaribi35@gmail.com>
2025-11-15 14:32:36 +0100

    ajout Hello.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 
 import './App.css'
+import Hello from './Hello'
 
 function App() {
   
 
   return (
     <>
-      
+      <Hello></Hello>
     </>
   )
 }
---
+++ src/Hello.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hello = () => {
+  return (
+    <div>
+      <p>Hello, world!</p>
+    </div>
+  )
+}
+
+export default Hello
